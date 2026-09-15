facdd5ba4065963e6319fb09e84f7bf47e1fa6a1
Tommaso Gambro <tg2016@hw.ac.uk>
2026-09-15 21:51:37 +0100

    Basics Made
    Basic template made (taskbar & multiple empty pages) - Claude used for css

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,51 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
+  /*
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  */
+ const [activePage, setActivePage] = useState<string>("pg1"); // used for changing which div to show
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
+      <div>
+        <h1>Welcome to KBR</h1>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
+      <div id="taskbar">
+        <p id="tbTitle">KBR</p>
+        <div id="taskbarBtns">
+          <button id="tbBt1" className={activePage === "pg1" ? "active" : ""} onClick={() => setActivePage("pg1")}>Main</button>
+          <button id="tbBt2" className={activePage === "pg2" ? "active" : ""} onClick={() => setActivePage("pg2")}>Key Pair Gen</button>
+          <button id="tbBt3" className={activePage === "pg3" ? "active" : ""} onClick={() => setActivePage("pg3")}>Page3</button>
+        </div>
+      </div>
+
+      
+      {activePage === "pg1" && (
+        <div id="pg1Main">
+          <h1>Page 1</h1>
+        </div>
+      )}{/* end of page 1 */}
+
+
+      {activePage === "pg2" && (
+        <div id="pg2Main">
+          <h1>Page 2</h1>
+        </div>
+      )}{/* end of page 2 */}
+
+
+      {activePage === "pg3" && (
+        <div id="pg3Main">
+          <h1>Page 3</h1>
+        </div>
+      )}{/* end of page 3 */}
+
     </main>
   );
 }
